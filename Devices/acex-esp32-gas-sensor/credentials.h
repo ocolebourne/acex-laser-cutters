db4a6f8d29dfd0cd8f3fee19ac526cf17dcd621c
Oliver Colebourne <57845551+ocolebourne@users.noreply.github.com>
2021-11-16 14:10:08 +0000

    gas sensor credentials initial commit

---
+++ Devices/acex-esp32-gas-sensor/credentials.h
@@ -0,0 +1,7 @@
+//Replace with your SSID and enterprise wifi credentials
+
+#define SSID "SSID"
+#define EAP_ID "EAP_ID"
+#define EAP_USERNAME "EAP_USERNAME"
+#define EAP_PASSWORD "PASSWORD"
+#define API_SECRET "PASSWORD"
